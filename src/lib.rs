4c8001c01120506013e9ffb5807aaacf3f25fc2d
Waleed Samy <waleedsamygd@protonmail.com>
2020-11-16 16:08:15 +0100

    use iterators to go through the text

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! minigrep
+//! search for string in a file
+//!
+
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -10,18 +14,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            Err("usage: minigrep file regex")
-        } else {
-            let is_case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
-            Ok(Config {
-                query: args[1].to_string(),
-                filename: args[2].to_string(),
-                case_sensitive: is_case_sensitive,
-            })
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(query) => query,
+            None => return Err("missing query"),
+        };
+        let filename = match args.next() {
+            Some(v) => v,
+            None => return Err("missing filename"),
+        };
+        let is_case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query: query,
+            filename: filename,
+            case_sensitive: is_case_sensitive,
+        })
     }
 }
 
@@ -41,24 +49,31 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
+/// search case insensitive
+///
+/// # Example
+///
+/// ```
+/// let query = "safe";
+/// let content = "\n
+/// Rust:
+/// safe, fast, productive.
+/// Pick three.";
+/// let expected = vec!["safe, fast, productive."];
+/// assert_eq!(expected, minigrep::search_case_insensitive(query,content));
+/// ```
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-    results
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,10 @@ use std::error::Error;
 use std::process;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = env::args().collect::<Vec<String>>();
-    let config = Config::new(&args).unwrap_or_else(|e| {
+    let config = Config::new(env::args()).unwrap_or_else(|e| {
         eprintln!("Problem: {}", e);
         process::exit(1);
     });
-
-    println!("config query: {:}", config.query);
-    println!("config filename: {:}", config.filename);
-    println!("");
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Problem: {}", e);
@@ -20,28 +15,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use minigrep::Config;
-    #[test]
-    fn test_valid_config() -> Result<(), &'static str> {
-        let args = &vec![
-            "minigrep".to_string(),
-            "filename".to_string(),
-            "query".to_string(),
-        ];
-        Config::new(&args).map(|_| ())
-    }
-
-    #[test]
-    fn test_invalid_config() -> Result<(), &'static str> {
-        let args = &vec!["minigrep".to_string(), "filename".to_string()];
-        let config = Config::new(&args);
-        match config {
-            Ok(_) => Err("shouldn't happen!"),
-            Err(_) => Ok(()),
-        }
-    }
 }
